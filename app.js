ac31e2ac8fc8647b55e365995d7ec335a56b0d20
Shohanur Rahman <shohanurr490@gmail.com>
2025-06-25 22:39:52 +0600

    refactor health check response message

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const express = require("express");
 require("dotenv").config();
 
-// Initialize the app
 const app = express();
-
-// Define the port
 const PORT = process.env.PORT || 3000;
 
-// Health check route
 app.get("/health", (req, res) => {
-  res.status(200).json({ status: "UP", message: "running", port: PORT });
+  res.status(200).json({ message: "Server is running", port: PORT });
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} `);
 });
