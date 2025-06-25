2481c894143c0def620b78c6a2702dd247b8aa1c
Shohanur Rahman <shohanurr490@gmail.com>
2025-06-25 22:28:01 +0600

    refactor health check response message

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 // Health check route
 app.get("/health", (req, res) => {
-  res
-    .status(200)
-    .json({ status: "UP", message: "Service is running", port: PORT });
+  res.status(200).json({ status: "UP", message: "running", port: PORT });
 });
 
 // Start the server
