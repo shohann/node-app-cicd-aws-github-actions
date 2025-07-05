95feabedd2adea87a28be8d8e1fbdefc3e801a02
Shohanur Rahman <shohanurr490@gmail.com>
2025-07-05 20:19:25 +0600

    refactor deployment workflow and update server startup message

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.get("/health", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} `);
+  console.log(`Server new is running on port ${PORT} `);
 });
